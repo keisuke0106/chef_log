00ac33c904b24d25d3b5bba280580ba1b2454b0b
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-09-08 17:29:56 +0900

    user編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+
+
+Refile.secret_key = '67bb1773ce445ccbe414230b307f243f6763f0039e08fd3313011fcc8ec0ba6e687e0bf65e49cb420a6763576b9a7ed3247522dbaede89ca57a986f310f85d7b'
