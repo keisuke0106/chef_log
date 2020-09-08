0ab1335d5dd7439bbcea0d6f63488794f65f2362
keisuke0106 <68574143+keisuke0106@users.noreply.github.com>
2020-09-08 20:43:35 +0900

    投稿と編集の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,11 +3,33 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
+    @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+    @recipe.save
+    redirect_to recipe_path(@recipe)
+  end
+
+
   def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :image)
   end
 end
